0ee7842acb692c3ac640794c661162da1d6595d8
strongeron <strongeron@gmail.com>
2015-09-16 23:12:08 +0300

    Mongol
    Adding jquery

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ // когда DOM будет готов
+	
+	// $('li').each(function(){
+	// 	// console.log( $(this) .text() );
+	// $(this).html('йцу<br>кен');
+	// });
+	 $("#owl").owlCarousel({
+	 	items:4
+	 });
+});
