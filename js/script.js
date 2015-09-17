09b414b4fdf3ae798cc84ac206603502709bb4d7
strongeron <strongeron@gmail.com>
2015-09-18 00:48:55 +0300

    Version 18.09
    Some slider and no count

--- js/script.js
+++ js/script.js
@@ -6,5 +6,17 @@ $(document).ready(function(){ // когда DOM будет готов
 	// });
 	 $("#owl").owlCarousel({
 	 	items:4
+	 	
 	 });
 });
+var newYear = new Date(); 
+newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
+$('#defaultCountdown').countdown({until: newYear}); 
+ 
+$('#removeCountdown').click(function() { 
+    var destroy = $(this).text() === 'Remove'; 
+    $(this).text(destroy ? 'Re-attach' : 'Remove'); 
+    $('#defaultCountdown').countdown(destroy ? 'destroy' : {until: newYear}); 
+});
+$.countdown.setDefaults({description: 'Until liftoff'});
+$(selector).countdown({since: new Date(2014, 12-1, 25)});
